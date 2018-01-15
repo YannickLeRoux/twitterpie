0171368c153da9a2ac1d50aa29614f56b45ef94c
YannickLeRoux <rasyann@hotmail.com>
2018-01-15 18:50:05 -0500

    Edit README.md and cleaned code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# twitterpie
+## Twitterpie
+
+# Web App made with Flask, Tweepy and Chart.js
+
+This actually my first web app, so the code is going to get better with time
+(hopefully!)
+
+
+I used the file `'twitter_parser/blacklist.txt'` to remove from results the words that would be
+meaningless, like "they" "but" "although" etc, so feel free to add some more, or words from other languages too.
--- app/routes.py
+++ app/routes.py
@@ -15,24 +15,16 @@ def index():
 @app.route("/piechart/<twitterhandle>")
 def pie(twitterhandle):
 
-
-
-    
     from app.twitter_parser import twitter_parser as tp
 
     alltweets = tp.get_all_tweets(twitterhandle)
     word_dict = tp.word_count(alltweets)
     keywords_dict = tp.top10words(word_dict)
 
-    
-
-#     fig.suptitle('@' +str(user_name) + ' 10 Most Used Words on Twitter\n'+
-#             str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
     values = [sizes for labels, sizes in keywords_dict]
     labels = [labels for labels, sizes in keywords_dict]
     colors = [ "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA","#ABCDEF", "#DDDDDD",
             "#ABCABC", "#CAABB9", "#46F7F3", "#EFCDAB"]
-
 
     # Render the Template
     return render_template('pie.html', values=values, labels=labels,
